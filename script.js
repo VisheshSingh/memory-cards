df751ea1cc4f061eb19500a8e201cf02981289ac
Vishesh Singh <VisheshSingh16@gmail.com>
2020-03-03 17:55:14 -0600

    implementing localStorage

--- script.js
+++ script.js
@@ -8,20 +8,13 @@ const questionEl = document.getElementById('question');
 const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearCardBtn = document.getElementById('clear');
-const addContainer = document.getElementById('cards-container');
+const addContainer = document.getElementById('add-container');
 
 let currentActiveCard = 0;
 
 const cardsEl = [];
 
-const cardsData = [
-  { question: 'What must a variable begin with?', answer: 'A letter, $ or _' },
-  { question: 'What is a variable?', answer: 'A container for piece of data' },
-  {
-    question: 'What is React?',
-    answer: 'A popular JS library for building user interfaces'
-  }
-];
+const cardsData = getCardsData();
 
 function createCards() {
   cardsData.forEach((data, index) => createCard(data, index));
@@ -77,4 +70,44 @@ nxtBtn.addEventListener('click', () => {
   }
   cardsEl[currentActiveCard].className = 'card active';
   updateCurrentText();
+});
+
+function getCardsData() {
+  const cards = JSON.parse(localStorage.getItem('cardsData'));
+  return cards === null ? [] : cards;
+}
+
+showBtn.addEventListener('click', () => {
+  addContainer.classList.add('show');
+});
+
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'));
+
+addCardBtn.addEventListener('click', () => {
+  const question = questionEl.value;
+  const answer = answerEl.value;
+
+  if (question.trim() && answer.trim()) {
+    const newCard = { question, answer };
+
+    createCard(newCard);
+    questionEl.value = '';
+    answerEl.value = '';
+
+    addContainer.classList.remove('show');
+
+    cardsData.push(newCard);
+    setCardsData(cardsData);
+  }
+});
+
+function setCardsData(cardsData) {
+  localStorage.setItem('cardsData', JSON.stringify(cardsData));
+  location.reload();
+}
+
+clearCardBtn.addEventListener('click', () => {
+  localStorage.removeItem('cardsData');
+  cardsContainer.innerHTML = '';
+  location.reload();
 });
